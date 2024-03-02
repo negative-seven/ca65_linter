85862c1e3efd5fb8699c4c51f9543af6266091c0
negative-seven <21268649+negative-seven@users.noreply.github.com>
2024-03-02 02:20:44 +0100

    Output user-friendly context for rule violations

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ mod ast;
 
 use ast::{
     ExplicitLabel, Expression, ImplicitLabel, InstructionOperandKind, NumberLiteralBase, Span,
-    Statement,
+    Spanned, Statement,
 };
 use lalrpop_util::lalrpop_mod;
 use std::{error::Error, io::Read};
 
-use crate::ast::Spanned;
-
 lalrpop_mod!(pub ca65);
 
+#[allow(clippy::too_many_lines)] // TODO
 fn main() -> Result<(), Box<dyn Error>> {
     let mut arguments = std::env::args().skip(1);
     let Some(source_filename) = arguments.next() else {
@@ -21,6 +20,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut source_file = std::fs::File::open(source_filename)?;
     let mut source = String::new();
     source_file.read_to_string(&mut source)?;
+    source.push('\n'); // hack for correct parsing, as LALRPOP does not support the $ regex
+                       // metacharacter
 
     let parser = ca65::StatementsParser::new();
     let statements = match parser.parse(&source) {
@@ -75,11 +76,54 @@ fn main() -> Result<(), Box<dyn Error>> {
         });
     }
 
+    let source_line_character_ranges = {
+        let newline_indices = source.char_indices().filter(|c| c.1 == '\n').map(|c| c.0);
+        let start_indices = std::iter::once(0).chain(newline_indices.clone().map(|i| i + 1));
+        let end_indices = newline_indices.chain(std::iter::once(source.len()));
+        start_indices.zip(end_indices).map(|(a, b)| a..b)
+    };
+
+    #[allow(clippy::items_after_statements)]
+    struct SourceLine<'a> {
+        text: &'a str,
+        character_index: usize,
+        line_index: usize,
+    }
+
+    let mut source_by_lines = Vec::new();
+    for (line_index, character_index_range) in source_line_character_ranges.enumerate() {
+        let text = &source[character_index_range.clone()];
+        let text = text.strip_suffix('\r').unwrap_or(text);
+        source_by_lines.push(SourceLine {
+            text,
+            character_index: character_index_range.start,
+            line_index,
+        });
+    }
+
+    rule_violations.sort_by_key(|v| v.span.start);
     for rule_violation in rule_violations {
+        let line = source_by_lines
+            .iter()
+            .rev()
+            .find(|line| rule_violation.span.start >= line.character_index)
+            .unwrap();
+
         println!("rule violation: {}", rule_violation.description);
         println!(
-            "at: {}..{}",
-            rule_violation.span.start, rule_violation.span.end
+            "at line {}, position {}",
+            line.line_index + 1,
+            rule_violation.span.start - line.character_index,
+        );
+        println!("{}", line.text);
+        let rule_violation_span_within_line = Span {
+            start: rule_violation.span.start - line.character_index,
+            end: rule_violation.span.end - line.character_index,
+        };
+        println!(
+            "{}{}",
+            " ".repeat(rule_violation_span_within_line.start),
+            "^".repeat(rule_violation_span_within_line.end - rule_violation_span_within_line.start)
         );
         println!();
     }
